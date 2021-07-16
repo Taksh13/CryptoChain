5f25ab44ad0a7607d67926208b8b66a7b40958ac
Taksh13 <ptaksh13@gmail.com>
2021-07-16 17:21:20 -0400

    production code

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const isDevelopment = process.env.ENV === 'development';
 
 const REDIS_URL = isDevelopment ?
     'redis://127.0.0.1:6379' :
-    'redis://h:p05f9a274bd0e2414e52cb9516f8cbcead154d7d61502d32d9750180836a7cc05@ec2-34-225-229-4.compute-1.amazonaws.com:19289'
+    'redis://:p9d9456fbaa5314ebf40c0c497914badc6a5f984d08931f432724846da2b5d8a8@ec2-52-207-52-141.compute-1.amazonaws.com:22579'
 const DEFAULT_PORT = 3000;
 const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
